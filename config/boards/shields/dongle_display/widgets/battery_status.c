dbf99c1690e196eb6c1467f12be604a1494056cf
Val Nov <val.nov.ua@gmail.com>
2024-08-19 13:48:26 +0300

    aligh_inndicators

--- config/boards/shields/dongle_display/widgets/battery_status.c
+++ config/boards/shields/dongle_display/widgets/battery_status.c
@@ -139,8 +139,8 @@ int zmk_widget_dongle_battery_status_init(struct zmk_widget_dongle_battery_statu
 
         lv_canvas_set_buffer(image_canvas, battery_image_buffer[i], 5, 8, LV_IMG_CF_TRUE_COLOR);
 
-        lv_obj_align(image_canvas, LV_ALIGN_TOP_LEFT, i * 50, 0);
-        lv_obj_align(battery_label, LV_ALIGN_TOP_LEFT, i * 10, 0);
+        lv_obj_align(image_canvas, LV_ALIGN_TOP_LEFT, i * 20, 0);
+        lv_obj_align(battery_label, LV_ALIGN_TOP_LEFT, i * 60, 0);
 
         lv_obj_add_flag(image_canvas, LV_OBJ_FLAG_HIDDEN);
         lv_obj_add_flag(battery_label, LV_OBJ_FLAG_HIDDEN);
